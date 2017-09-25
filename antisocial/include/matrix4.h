e6d4e140d76a9d3099c2804c2e3a6392a4fbd9e4
MachineMitch21 <mitchschutt@outlook.com>
2017-09-25 14:15:49 -0400

    Added source file for matrix4

---
+++ antisocial/include/matrix4.h
@@ -0,0 +1,20 @@
+#ifndef __MATRIX4_H__
+#define __MATRIX4_H__
+
+namespace antisocial {
+
+	class Matrix4 {
+
+	public:
+		Matrix4();
+		~Matrix4();
+
+	private:
+		float elements[16];
+	};
+
+}
+
+
+
+#endif //__MATRIX4_H__
---
+++ antisocial/src/matrix4.cpp
@@ -0,0 +1,15 @@
+
+#include "matrix4.h"
+
+using antisocial::Matrix4;
+
+
+Matrix4::Matrix4()
+{
+	
+}
+
+Matrix4::~Matrix4()
+{
+
+}
