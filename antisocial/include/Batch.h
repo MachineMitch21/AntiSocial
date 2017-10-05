6bb63bbd00a1e1be7c95d098340df59ea664da3f
MachineMitch21 <mitchschutt@outlook.com>
2017-10-04 21:13:55 -0400

    Added Batch.h for instance renderering at some point

---
+++ antisocial/include/Batch.h
@@ -0,0 +1,21 @@
+
+#ifndef __BATCH_H__
+#define __BATCH_H__
+
+namespace antisocial
+{
+
+    class Batch
+    {
+    public:
+        Batch();
+        ~Batch();
+
+    private:
+
+    };
+
+}
+
+
+#endif // __BATCH_H__
