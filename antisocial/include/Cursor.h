2b8802a6fe85235b8c036e2bc491cfd2f9051c30
MachineMitch21 <mitchschutt@outlook.com>
2017-10-05 14:18:47 -0400

    Added Cursor.h

---
+++ antisocial/include/Cursor.h
@@ -0,0 +1,20 @@
+
+#ifndef __CURSOR_H__
+#define __CURSOR_H__
+
+namespace antisocial
+{
+    namespace input
+    {
+        class Cursor
+        {
+        public:
+
+        private:
+            GLFWcursor* _cursor;
+        };
+    }
+}
+
+
+#endif // __CURSOR_H__
